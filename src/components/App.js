ea9b5bddd0740f1d084fa24c2ba785e1e1407546
Joe Dietrich <joe@joedietrich.net>
2021-09-19 22:42:33 -0400

    commit tree d129aa855d724f39e67fab847dec020636879802

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Segment } from "semantic-ui-react";
+import { splitAndCapitalize } from "../services/Capitalize";
+import { Log } from "../services/Log";
 import "../stylesheets/App.css";
+import Headquarters from "./Headquarters";
+import WestworldMap from "./WestworldMap";
 
 function App() {
+  const [hosts, setHosts] = useState([]);
+  const [selectedHostId, setSelectedHostId] = useState(0);
+  const [areas, setAreas] = useState([]);
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/hosts")
+      .then((res) => res.json())
+      .then(setHosts);
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/areas")
+      .then((res) => res.json())
+      .then(setAreas);
+  }, []);
+
+  function handleUpdateHost(newAttribute) {
+    const updatingHost = { ...hosts.find((host) => host.id === selectedHostId) };
+    if (newAttribute.hasOwnProperty("area")) {
+      const currentHostsInNewArea = hosts.filter((host) => host.area === newAttribute.area);
+      const newHostCount = currentHostsInNewArea.length + 1;
+      if (newHostCount > areas.find((area) => area.name === newAttribute.area).limit) {
+        setLogs((logs) => [Log.error(`Too many hosts. Cannot add ${updatingHost.firstName} to ${newAttribute.area}`), ...logs]);
+      } else if (newAttribute.area === "under_construction" && !hosts.find((host) => host.id === selectedHostId).authorized) {
+        setLogs((logs) => [Log.error(`PERMISSION DENIED. ${updatingHost.firstName} IS UNAUTHORIZED`), ...logs]);
+      } else {
+        updateHost(newAttribute, updatingHost);
+      }
+    } else {
+      updateHost(newAttribute, updatingHost);
+    }
+  }
+
+  function handleToggleActivateAll(shouldActivateAll) {
+    if (shouldActivateAll) {
+      setLogs((logs) => [Log.warn("Activating all hosts!"), ...logs]);
+    } else {
+      setLogs((logs) => [Log.notify("Decommissioning all hosts."), ...logs]);
+    }
+    const updateIds = hosts.filter((host) => (shouldActivateAll ? !host.active : host.active));
+    updateIds.forEach((host) => updateHost({ active: shouldActivateAll }, host));
+  }
+
+  function updateHost(newAttribute, updatingHost) {
+    const updateHostId = updatingHost.id;
+    setHosts((hosts) => hosts.map((host) => (host.id === updateHostId ? { ...host, ...newAttribute } : host)));
+
+    fetch(`http://localhost:3001/hosts/${updateHostId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newAttribute),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Cannot Update");
+        if (newAttribute.hasOwnProperty("area"))
+          setLogs([Log.notify(`${updatingHost.firstName} set in area ${splitAndCapitalize(newAttribute.area)}`), ...logs]);
+        if (newAttribute.hasOwnProperty("active")) {
+          newAttribute.active
+            ? setLogs([Log.warn(`Activated ${updatingHost.firstName}`), ...logs])
+            : setLogs((logs) => [Log.notify(`Decommissioned ${updatingHost.firstName}`), ...logs]);
+        }
+      })
+      .catch((err) => {
+        setLogs((logs) => [Log.error("There was a problem updating " + updatingHost.firstName), ...logs]);
+        setHosts((hosts) => hosts.map((host) => (host.id === updateHostId ? updateHost : host)));
+      });
+  }
+
+  const activeHosts = hosts.filter((host) => host.active === true);
+
   return (
     <Segment id="app">
-      {/* What components should go here? Check out Checkpoint 1 of the Readme if you're confused */}
+      <WestworldMap areas={areas} activeHosts={activeHosts} selectedHostId={selectedHostId} setSelectedHostId={setSelectedHostId} />
+      <Headquarters
+        areas={areas}
+        hosts={hosts}
+        logs={logs}
+        selectedHostId={selectedHostId}
+        setSelectedHostId={setSelectedHostId}
+        onUpdateHost={handleUpdateHost}
+        onToggleActivateAll={handleToggleActivateAll}
+      />
     </Segment>
   );
 }
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Segment, Image } from "semantic-ui-react";
 import * as Images from "../services/Images";
+import HostInfo from "./HostInfo";
 
-function Details() {
+function Details({ selectedHost, areas, onUpdateHost = f => f }) {
   // We'll render the logo if no host is selected. But if a host does get selected....
   // Watch the video to see how this works in the app.
 
   return (
     <Segment id="details" className="HQComps">
-      {true ? <Image size="medium" src={Images.westworldLogo} /> : null}
+      {selectedHost ? <HostInfo host={selectedHost} areas={areas} onUpdateHost={onUpdateHost} /> : <Image size="medium" src={Images.westworldLogo} />}
     </Segment>
   );
 }
